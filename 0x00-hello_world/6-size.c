be991db0af3a7f12c2422f16fa67c9c1a1dbc0d0
tebza24 <tebogom247@gmail.com>
2023-02-25 11:14:11 -0800

    updated

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,5 +10,8 @@ int main(void)
 	printf("Size of a long int: %d byts(s)\n", sizeof(long int));
 	printf("Size of a long long int: %d byts(s)\n", sizeof(long long int));
 	printf("Size of a float: %d byts(s)\n", sizeof(float));
+	
+	fprintf(stderr, "This is an error messeage.\n");
+
 	return (0);
 }
