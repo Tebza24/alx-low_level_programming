d940832dd90ba92b05d1ac5951fd0a439539f101
Tebza24 <motaung.tebogo247@gmail.com>
2023-07-04 12:05:35 -0700

    update 101

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,18 +1,9 @@
-section .data
-	format db 'Hello, Holberton', 0
-
-section .text
-	extern printf
-
-global main
+   global    main
+          extern    printf
 main:
-	; Call printf
-	mov edi, format
-	xor eax, eax
-	call printf
-
-	; Exit the program
-	xor edi, edi
-	xor eax, eax
-	mov eax, 60
-	syscall
+	  mov   edi, format
+	  xor   eax, eax
+	  call  printf
+	  mov 	eax, 0
+	  ret
+format: db `Hello, Holberton\n`,
